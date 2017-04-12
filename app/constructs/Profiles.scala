2999196e1176124eb382d7965e5e8f4d365e40ca
Jordan Dodson <jordandodson22@gmail.com>
2017-04-12 04:28:51 -0500

    moving to the CSS-styled version of highcharts. now, media queries are used to set chart font sizes

--- app/constructs/Profiles.scala
+++ app/constructs/Profiles.scala
@@ -24,13 +24,14 @@ import play.api.libs.json.Json
   * @param imdb          Url of IMDb profile
   * @param gyroscope     Url of Gyroscope profile
   * @param duolingo      Url of Duolingo profile
+  * @param wordpress     Url of Wordpress profile
   */
 case class Profiles(github: String, twitter: String, linkedin: String,
                     stackexchange: String, googleplus: String, ycombinator: String,
                     goodreads: String, codewars: String, freecodecamp: String,
                     quora: String, pinterest: String, facebook: String,
                     codecademy: String, coursera: String, imdb: String,
-                    gyroscope: String, duolingo: String)
+                    gyroscope: String, duolingo: String, wordpress: String)
 
 /**
   * Companion object where handlers and writers are defined
@@ -39,7 +40,7 @@ object Profiles {
 
   import reactivemongo.bson.Macros
 
-  val emptyProfiles = Profiles("", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "")
+  val emptyProfiles = Profiles("", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "")
 
   // Implicitly convert to/from BSON
   implicit val ProfilesHandler = Macros.handler[Profiles]
