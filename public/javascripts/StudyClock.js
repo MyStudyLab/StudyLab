6284c5eca8bfd59e0fc204024f37cc77fc728956
Jordan Dodson <jordandodson22@gmail.com>
2017-04-20 21:34:48 -0500

    refactoring the stopwatch

--- public/javascripts/StudyClock.js
+++ public/javascripts/StudyClock.js
@@ -16,26 +16,54 @@ function padString(str, padStr, len) {
     return padStr.repeat(numRepeats) + padStr.substr(0, remainder) + str;
 }
 
+/**
+ * A simple stopwatch
+ *
+ * @param {string} elementId -
+ * @member {int} intervalID -
+ * @member {int} startTime - The time that this stopwatch begins (milliseconds since the epoch)
+ * @constructor
+ */
+function Stopwatch(elementID) {
 
-function StopWatch(start = -1) {
+    this.elementID = elementID;
+    this.intervalID = -1;
+    this.startTime = -1;
 
-    if (start < 0) {
-        this.start = Date.now();
-    } else {
-        this.start = start;
-    }
+    this.start = function () {
 
-    this.restart = function () {
-        this.start = Date.now();
+        this.startTime = Date.now();
+
+        this.intervalID = setInterval(function () {
+            document.getElementById(elementID).innerText = stopwatch.display();
+        }, 1000);
+
     };
 
-    this.set = function (start) {
-        this.start = start;
+    this.startFrom = function (startTime) {
+
+        this.startTime = startTime;
+
+        //let elementId = this.elementID;
+
+        this.intervalID = setInterval(function () {
+            document.getElementById(elementID).innerText = stopwatch.display();
+        }, 1000,);
+
+    };
+
+    this.reset = function () {
+        document.getElementById(this.elementID).innerText = "00:00:00";
+        clearInterval(this.intervalID);
+    };
+
+    this.displayZero = function () {
+        document.getElementById(this.elementID).innerText = "00:00:00";
     };
 
     this.display = function () {
 
-        const elapsed = Math.round((Date.now() - this.start) / 1000);
+        const elapsed = Math.round((Date.now() - this.startTime) / 1000);
 
         const hours = Math.floor(elapsed / 3600);
 
