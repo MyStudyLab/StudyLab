b7360b6922fbf73f564880ecb3c677dad0899fdd
Jordan Dodson <jordandodson22@gmail.com>
2017-11-11 05:25:22 -0600

    adding a temporary fix for submission control-panel hiding

--- public/jsx/JournalSubmissionForm.jsx
+++ public/jsx/JournalSubmissionForm.jsx
@@ -33,12 +33,12 @@ export default class JournalSubmissionForm extends React.Component {
                 <div id="journalSubmissionControl">
 
                     <button onClick={this.props.handleGeoToggle} id="journalSubmissionGeoButton"
-                            className={`${this.props.useGeo ? "active" : ""} fa fa-globe fa-lg transparentButton`}
+                            className={`${this.props.useGeo ? "active" : ""} ${this.props.writingMode ? "" : "vanish"} fa fa-globe fa-lg transparentButton`}
                     />
 
 
                     <button type="submit" id="journalSubmissionButton"
-                            className="fa fa-paper-plane-o fa-lg transparentButton"
+                            className={`fa fa-paper-plane-o fa-lg transparentButton ${this.props.writingMode ? "" : "vanish"}`}
                             disabled={this.props.text.length === 0}
                     />
                 </div>
